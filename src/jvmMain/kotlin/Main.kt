5d281e53517acf95a8f17c42f69e644c7f069403
幻弦让叶 <1248389474@qq.com>
2023-02-06 15:34:40 +0800

    调试命令

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,20 +1,32 @@
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.launch
+import utils.AdbHelper
 
 @Composable
 @Preview
 fun App() {
 
+    var tips by remember { mutableStateOf("") }
+    val coroutine = rememberCoroutineScope()
     MaterialTheme {
+        Column {
+            Button(onClick = {
+                coroutine.launch {
+                    val result = AdbHelper.version()
+                    tips = result.stdout + "\n" + result.stderr
+                }
+            }) {
+                Text("test")
+            }
+            Text(tips)
+        }
     }
 }
 
