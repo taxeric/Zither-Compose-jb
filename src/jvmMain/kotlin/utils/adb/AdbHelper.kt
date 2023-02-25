4531651de08d8d1224d2798ba3554855592122f1
幻弦让叶 <1248389474@qq.com>
2023-02-25 13:00:00 +0800

    新增部分命令

--- src/jvmMain/kotlin/utils/adb/AdbHelper.kt
+++ src/jvmMain/kotlin/utils/adb/AdbHelper.kt
@@ -15,6 +15,16 @@ class AdbHelper {
 
     suspend fun devices() = AdbProcess.exec(listOf("devices"))
 
+    suspend fun deviceModel() = AdbProcess.exec(listOf("shell", "getprop", "ro.product.model"))
+
+    suspend fun deviceBrand() = AdbProcess.exec(listOf("shell", "getprop", "ro.product.brand"))
+
+    suspend fun deviceName() = AdbProcess.exec(listOf("shell", "getprop", "ro.product.name"))
+
+    suspend fun deviceBuildSDK() = AdbProcess.exec(listOf("shell", "getprop", "ro.build.version.sdk"))
+
+    suspend fun deviceBuildRelease() = AdbProcess.exec(listOf("shell", "getprop", "ro.build.version.release"))
+
     suspend fun installApk(path: String) = AdbProcess.exec(listOf("install", path))
 
     suspend fun uninstallApk(packageName: String) = AdbProcess.exec(listOf("uninstall", packageName))
@@ -24,4 +34,6 @@ class AdbHelper {
     suspend fun clearApkDataByShell(packageName: String) = AdbProcess.exec(listOf("pm", "clear", packageName), true)
     
     suspend fun screenSize() = AdbProcess.exec(listOf("shell", "wm", "size"))
+
+    suspend fun screenDensity() = AdbProcess.exec(listOf("shell", "wm", "density"))
 }
